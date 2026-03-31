1e7a7787795949d46ecc317367fe50483849b6e4
rl-hicks <210308863+rl-hicks@users.noreply.github.com>
2026-03-30 21:47:44 -0700

    Daniels's main

--- src/main.cpp
+++ src/main.cpp
@@ -4,90 +4,85 @@
 #include <sstream>
 #include <vector>
 
-
 #include "DAG.h"
 
 using namespace std;
 
-
-
 vector<Client> readClients(const string& filename) {
     vector<Client> clients;
-
     ifstream file(filename);
 
     if (!file) {
-        cerr << "Error in Function: readClient()... opening file: " << filename << endl;
+        cerr << "Error opening file: " << filename << endl;
         return clients;
     }
 
     string line;
-
     while (getline(file, line)) {
-
-
         stringstream ss(line);
-
         Client c{};
         ss >> c.start >> c.end >> c.revenue;
-
         clients.push_back(c);
     }
 
     return clients;
 }
 
-
 int main(int argc, char* argv[]) {
-
     if (argc < 2) {
         cerr << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
 
-    string filename = "data/" + string(argv[1]);
-
-    cout << "Filename: " << filename << endl;
-
-
+    string inputFile = argv[1];
+    string filename = "data/" + inputFile;
 
     vector<Client> clients = readClients(filename);
 
-    for (Client& c : clients) {
-        cout << c.end << endl << c.revenue << endl << c.start << endl << endl;
+    if (clients.empty()) {
+        cerr << "No clients loaded from file: " << filename << endl;
+        return 1;
     }
 
     DAG dag(clients);
-
-    cout << "\nGraph:\n";
-    dag.printGraph();
-
-    vector<int> topo = dag.topologicalSort();
-
-    cout << "\nTopological Order:\n";
-    for (int v : topo) {
-        cout << v << " ";
-    }
-    cout << endl;
-
-    cout << "START = " << dag.START << ", END = " << dag.END << endl;
-
-
-    auto result = dag.computeOptimal();
+    pair<int, vector<int>> result = dag.computeOptimal();
 
     int maxRevenue = result.first;
     vector<int> path = result.second;
 
-    cout << "\nMax Revenue: " << maxRevenue << endl;
+    cout << "There are " << clients.size() << " clients in this file" << endl << endl;
+    cout << "Optimal revenue earned is " << maxRevenue << endl;
+    cout << "Clients contributing to this optimal revenue: ";
 
-    cout << "Selected Clients:\n";
-    for (int idx : path) {
-        cout << idx << ": "
-             << dag.clients[idx].start << ", "
-             << dag.clients[idx].end << ", "
-             << dag.clients[idx].revenue << endl;
+    for (int i = 0; i < path.size(); i++) {
+        cout << path[i] + 1;   // convert from 0-based to 1-based
+        if (i < path.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+
+    string outFile = "out" + inputFile;
+    ofstream out(outFile);
+
+    if (!out) {
+        cerr << "Could not create output file: " << outFile << endl;
+        return 1;
     }
 
+    out << "There are " << clients.size() << " clients in this file" << endl << endl;
+    out << "Optimal revenue earned is " << maxRevenue << endl;
+    out << "Clients contributing to this optimal revenue: ";
 
+    for (int i = 0; i < path.size(); i++) {
+        out << path[i] + 1;   // convert from 0-based to 1-based
+        if (i < path.size() - 1) {
+            out << ", ";
+        }
+    }
+    out << endl;
 
+    out.close();
+
+    return 0;
 }
